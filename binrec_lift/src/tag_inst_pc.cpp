c0126f5db518d1ee0da5b8cc778dfec2f526a05e
Adam Meily <adam.meily@trailofbits.com>
2022-03-22 17:32:54 -0400

    refactor how instruction start and last pc tags are applied so they are uniform for all basic blocks; fixes #135

--- binrec_lift/src/tag_inst_pc.cpp
+++ binrec_lift/src/tag_inst_pc.cpp
@@ -9,101 +9,86 @@ using namespace llvm;
 
 
 namespace {
-    void tag_pc_in_function(Function &f, GlobalVariable *pc, GlobalVariable *ret, MDNode *md)
+    /*
+     * Check if the @return_address global is updated prior to the given instruction
+     * within the same basic block. This method checks if a "store *, @return_address"
+     * occurs prior to the given instruction in the containing basic block.
+     *
+     * This method is used to filter PC values. We ignore PC values that occur after a
+     * store to @return_address because this heuristic is indicative of a function call
+     * or return.
+     */
+    bool follows_store_return_address(Instruction *inst, GlobalVariable *return_address)
     {
-        // We are only interested in the first blocks (before conditional branches). Other blocks
-        // will be handled separately.
-        auto bb = &f.getEntryBlock();
-        unsigned prev_pc = 0;             // the last stored @PC value
-        unsigned max_pc = 0;              // the current maximum @PC value in this function
-        bool last_inst_stored_pc = false; // the last instruction updated max_pc
-
-        // load the lastpc from the function, which may be already set
-        if (MDNode *mdlast = binrec::getBlockMeta(&f, "lastpc")) {
-            unsigned curlastpc =
-                cast<ConstantInt>(dyn_cast<ValueAsMetadata>(mdlast->getOperand(0))->getValue())
-                    ->getZExtValue();
-            if (curlastpc) {
-                max_pc = curlastpc;
-                prev_pc = curlastpc;
+        const Instruction *prev = inst->getPrevNonDebugInstruction();
+        while (prev) {
+            auto store = dyn_cast<StoreInst>(prev);
+            if (store && store->getPointerOperand() == return_address) {
+                return true;
             }
+
+            prev = prev->getPrevNonDebugInstruction();
         }
 
-        while (bb) {
-            for (auto &ins : *bb) {
-                if (auto store = dyn_cast<StoreInst>(&ins)) {
-                    auto target = store->getPointerOperand();
-                    last_inst_stored_pc = false;
-
-                    // NOTE (hbrodin): If we store to the return address the next PC store is the
-                    // callee, which is not of concern for this block so end here.
-                    if (target == ret) {
-                        assert(max_pc);
-                        binrec::setBlockMeta(&f, "lastpc", max_pc);
-                        return;
-                    }
-
-                    // From this point we are only interested in stores to @PC
-                    if (target != pc) {
-                        continue;
-                    }
-
-                    // This is a constant store to @PC. Mark it as inststart and
-                    // update lastpc for the block
-                    if (auto ci = dyn_cast<ConstantInt>(store->getValueOperand())) {
-                        store->setMetadata("inststart", md);
-
-                        unsigned current_pc = ci->getZExtValue();
-                        if (current_pc > max_pc) {
-                            // only update the max @PC stored in this function
-                            prev_pc = max_pc;
-                            max_pc = current_pc;
-                            last_inst_stored_pc = true;
-                        }
-                    }
-                } else {
-                    if (dyn_cast<ReturnInst>(&ins) && last_inst_stored_pc && prev_pc) {
-                        // This is a "ret" instruction. If the previous instruction
-                        // was "load @PC ...", then we ignore the last @PC value that
-                        // was used, because this PC is most likely a return address
-                        // and is not valid.
-                        //
-                        // See Github issue #90:
-                        // https://github.com/trailofbits/binrec-prerelease/issues/90
-                        DBG("ignoring last pc, likely a return: " << f.getName() << ": " << max_pc
-                                                                  << " -> " << prev_pc);
-                        max_pc = prev_pc;
-                    }
-                    last_inst_stored_pc = false;
-                }
-            }
-
-            auto term = bb->getTerminator();
-            if (auto br = dyn_cast<BranchInst>(term)) {
-                if (br->isUnconditional()) {
-                    bb = dyn_cast<BasicBlock>(br->getOperand(0));
-                    continue;
-                }
-            }
-            bb = nullptr;
-        }
-
-        assert(max_pc);
-        binrec::setBlockMeta(&f, "lastpc", max_pc);
+        return false;
     }
 
     void tag_pc(Module &m)
     {
+        MDNode *md = MDNode::get(m.getContext(), NULL);
+
         GlobalVariable *pc = m.getNamedGlobal("PC");
         // Used for for calls
         GlobalVariable *ret = m.getNamedGlobal("return_address");
 
-        MDNode *md = MDNode::get(m.getContext(), NULL);
-
-        for (auto &f : m) {
-            if (!binrec::is_lifted_function(f))
+        // iterate over all uses of the @PC global
+        for (Use &use : pc->uses()) {
+            // filter to only uses that are store const integer instructions within a
+            // lifted function that do not follow a store to @return_address.
+            auto store = dyn_cast<StoreInst>(use.getUser());
+            if (!store) {
                 continue;
-            tag_pc_in_function(f, pc, ret, md);
+            }
+
+            auto pc_value = dyn_cast<ConstantInt>(store->getValueOperand());
+            if (!pc_value) {
+                continue;
+            }
+
+            Function *func = store->getFunction();
+            if (!func || !binrec::is_lifted_function(*func)) {
+                continue;
+            }
+
+            if (follows_store_return_address(store, ret)) {
+                continue;
+            }
+
+            // this is the start of an instruction
+            store->setMetadata("inststart", md);
+
+            // Verify that the next instruction is not a return instruction. If it is,
+            // ignore this PC value.
+            auto next = store->getNextNonDebugInstruction();
+            if (next && isa<ReturnInst>(next)) {
+                continue;
+            }
+
+            // Get the last PC value for this function.
+            MDNode *mdlast = binrec::getBlockMeta(func, "lastpc");
+            unsigned current_pc = pc_value->getZExtValue();
+            unsigned lastpc = 0;
+
+            if (mdlast) {
+                lastpc =
+                    cast<ConstantInt>(dyn_cast<ValueAsMetadata>(mdlast->getOperand(0))->getValue())
+                        ->getZExtValue();
+            }
+
+            if (current_pc > lastpc) {
+                // update the lastpc metadata for the function
+                binrec::setBlockMeta(func, "lastpc", current_pc);
+            }
         }
     }
 } // namespace
